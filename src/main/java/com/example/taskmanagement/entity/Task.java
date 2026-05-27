f5f7e7d1005c8455701aac8f682f7f29239f5fba
abhishek <abhi.patel0604@gmail.com>
2026-05-27 15:42:40 +0530

    configured lombok

--- src/main/java/com/example/taskmanagement/entity/Task.java
+++ src/main/java/com/example/taskmanagement/entity/Task.java
@@ -10,8 +10,16 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Task {
 
     @Id
@@ -45,124 +53,6 @@ public class Task {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    public Task() {
-    }
-
-    private Task(Builder builder) {
-        this.id = builder.id;
-        this.title = builder.title;
-        this.description = builder.description;
-        this.status = builder.status;
-        this.priority = builder.priority;
-        this.dueDate = builder.dueDate;
-        this.createdDate = builder.createdDate;
-        this.updatedDate = builder.updatedDate;
-        this.completedDate = builder.completedDate;
-        this.progressPercentage = builder.progressPercentage;
-        this.employee = builder.employee;
-        this.category = builder.category;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public LocalDateTime getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDateTime dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(LocalDateTime updatedDate) {
-        this.updatedDate = updatedDate;
-    }
-
-    public LocalDateTime getCompletedDate() {
-        return completedDate;
-    }
-
-    public void setCompletedDate(LocalDateTime completedDate) {
-        this.completedDate = completedDate;
-    }
-
-    public Integer getProgressPercentage() {
-        return progressPercentage;
-    }
-
-    public void setProgressPercentage(Integer progressPercentage) {
-        this.progressPercentage = progressPercentage;
-    }
-
-    public Employee getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
     @PrePersist
     public void setCreatedDateBeforeSave() {
         if (createdDate == null) {
@@ -174,85 +64,5 @@ public class Task {
     @PreUpdate
     public void setUpdatedDateBeforeSave() {
         updatedDate = LocalDateTime.now();
-    }
-
-    public static class Builder {
-
-        private Long id;
-        private String title;
-        private String description;
-        private String status;
-        private String priority;
-        private LocalDateTime dueDate;
-        private LocalDateTime createdDate;
-        private LocalDateTime updatedDate;
-        private LocalDateTime completedDate;
-        private Integer progressPercentage;
-        private Employee employee;
-        private Category category;
-
-        public Builder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder title(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public Builder description(String description) {
-            this.description = description;
-            return this;
-        }
-
-        public Builder status(String status) {
-            this.status = status;
-            return this;
-        }
-
-        public Builder priority(String priority) {
-            this.priority = priority;
-            return this;
-        }
-
-        public Builder dueDate(LocalDateTime dueDate) {
-            this.dueDate = dueDate;
-            return this;
-        }
-
-        public Builder createdDate(LocalDateTime createdDate) {
-            this.createdDate = createdDate;
-            return this;
-        }
-
-        public Builder updatedDate(LocalDateTime updatedDate) {
-            this.updatedDate = updatedDate;
-            return this;
-        }
-
-        public Builder completedDate(LocalDateTime completedDate) {
-            this.completedDate = completedDate;
-            return this;
-        }
-
-        public Builder progressPercentage(Integer progressPercentage) {
-            this.progressPercentage = progressPercentage;
-            return this;
-        }
-
-        public Builder employee(Employee employee) {
-            this.employee = employee;
-            return this;
-        }
-
-        public Builder category(Category category) {
-            this.category = category;
-            return this;
-        }
-
-        public Task build() {
-            return new Task(this);
-        }
     }
 }
